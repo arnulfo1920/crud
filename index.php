17ce3d8da5512c18bb4080a9e444713431c18e8a
Arnulfo <perezalvareza.271@gmail.com>
2018-02-12 15:05:37 -0600

    agregue nueva pagina

--- index.php
+++ index.php
@@ -1,1 +1,137 @@
-index.php
+<?php
+	require 'conexion.php';
+	
+	$where = "";
+	
+	if(!empty($_POST))
+	{
+		$valor = $_POST['campo'];
+		if(!empty($valor)){
+			$where = "WHERE nombre LIKE '%$valor'";
+		}
+	}
+	$sql = "SELECT * FROM clientes $where";
+	$resultado = $mysqli->query($sql);
+	
+?>
+<html lang="es">
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<link href="css/bootstrap-theme.css" rel="stylesheet">
+		<script src="js/jquery-3.1.1.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>	
+	</head>
+	
+	<body>
+  
+
+
+		<header>
+	<?php 
+		require_once('header.php');
+	?>	
+</header>
+ 
+<section>	
+	<div class="container">
+	<?php 
+			// carga el archivo routing.php para direccionar a la página .php que se incrustará entre la header y el footer
+			require_once('index.php');
+	 ?>
+ 
+	</div>
+</section>
+
+		<div class="container">
+			<div class="row">
+				<h2 style="text-align:center">registro de clientes</h2>
+			</div>
+		
+			<div class="row">
+				<a href="nuevo.php" class="btn btn-success">Nuevo Registro</a>
+				
+				<form action="<?php $_SERVER['PHP_SELF']; ?>" method="POST">
+					<b>Nombre: </b><input type="text" id="campo" name="campo" />
+					<input type="submit" id="enviar" name="enviar" value="Buscar" class="btn btn-info" />
+				</form>	
+			</div>
+			
+			<br>
+			
+			<div class="row table-bordered">
+		
+				<table class="table table-striped">
+					<thead>
+						<tr>
+							<th>ID</th>
+							<th>Nombre</th>
+							<th>rfc</th>
+							<th>Telefono</th>
+							<th>email</th>
+							<th>estatus</th>
+							<th>direccion</th>
+							<th></th>
+							<th></th>
+						</tr>
+					</thead>
+					
+					<tbody>
+						<?php while($row = $resultado->fetch_array(MYSQLI_ASSOC)) { ?>
+							<tr>
+								<td><?=$row['id']?></td>
+								<td><?php echo $row['nombre']; ?></td>
+								<td><?php echo $row['rfc']; ?></td>
+								<td><?php echo $row['telefono']; ?></td>
+								<td><?php echo $row['email'];?></td>
+								<td><?php echo $row['estatus'];?></td>
+								<td><?php echo $row['direcccion'];?></td>
+								<td><a href="modificar.php?id=<?php echo $row['id']; ?>"><span class="btn btn-warning glyphicon glyphicon-pencil"></span></a></td>
+								<td><a href="#" data-href="eliminar.php?id=<?php echo $row['id']; ?>" data-toggle="modal" data-target="#confirm-delete"><span class="btn btn-danger glyphicon glyphicon-trash"></span></a></td>
+							</tr>
+						<?php } ?>
+					</tbody>
+				</table>
+			
+			</div>
+
+
+		</div>
+		
+		<!-- Modal -->
+		<div class="modal fade" id="confirm-delete" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+			<div class="modal-dialog">
+				<div class="modal-content">
+					
+					<div class="modal-header">
+						<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+						<h4 class="modal-title" id="myModalLabel">Eliminar Registro</h4>
+					</div>
+					
+					<div class="modal-body">
+						¿Desea eliminar este registro?
+					</div>
+					
+					<div class="modal-footer">
+						<button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+						<a class="btn btn-danger btn-ok">Delete</a>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		<script>
+			$('#confirm-delete').on('show.bs.modal', function(e) {
+				$(this).find('.btn-ok').attr('href', $(e.relatedTarget).data('href'));
+				
+				$('.debug-url').html('Delete URL: <strong>' + $(this).find('.btn-ok').attr('href') + '</strong>');
+			});
+		</script>	
+		<center><footer>
+	<?php 
+		include_once('footer.php');
+	?>
+</footer></center>
+	</body>
+
+</html>	
